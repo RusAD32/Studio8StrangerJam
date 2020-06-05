9e081e36cd9c271761290c120e921a73f4d8e9b8
TheProgrammer <popdragos021@gmail.com>
2020-06-05 23:15:59 +0300

    s
    s

---
+++ Assets/Scripts/rotateNdetect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class rotateNdetect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float _spd;
+    private PlayerDetector _playerDetector;
+    private Material _mat;
+
+    void Start()
+    {
+        _playerDetector = GetComponent<PlayerDetector>();
+        _mat = GetComponent<MeshRenderer>().material;
+        _spd = Random.value * 360;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(transform.up, _spd*Time.deltaTime);
+        if (_playerDetector.SeePlayer())
+        {
+            _mat.color = Color.red;
+        }
+        else
+        {
+            _mat.color = Color.gray;
+        }
+    }
+}
