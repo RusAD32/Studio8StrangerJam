445cfbb7f7eb174af969fd4d422405fd7e35f0b2
TheProgrammer <popdragos021@gmail.com>
2020-06-05 23:07:46 +0300

    no
    yes

---
+++ Assets/Scripts/Totems.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totems : MonoBehaviour
+{
+    public bool isTaken = false;
+
+    void Start()
+    {
+        isTaken = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            isTaken = true;
+        }
+    }
+}
